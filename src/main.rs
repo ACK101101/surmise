1536ad8d3c93e63cc572e3977bed99686e795830
ACK101101 <alexanderckumar@gmail.com>
2026-05-15 01:17:54 -0400

    refactor w windows

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use minifb::*;
+mod window;
+use window::{*};
 
 mod camera;
 use camera::{*};
 
 mod transform;
-use transform::{*};
 
 fn main() {
     env_logger::init(); 
@@ -12,47 +12,26 @@ fn main() {
     let mut camera = match Cam::new() {
         Ok(c) => c,
         Err(e) => {
-            eprintln!("Camera oopsie: {e}");
+            eprintln!("Cam oopsie: {e}");
             return;
         }
     };
 
-    const WINDOW_WIDTH: usize = 960;
-    const WINDOW_HEIGHT: usize = 540;
-    log::debug!("Window Dims: ({}, {})", WINDOW_WIDTH, WINDOW_HEIGHT);
-
-    let mut pixel_dims: Rect = Rect {
-        width: 32,
-        height: 16,
+    // let wins: Vec<Win> = Vec::new();
+    let mut win = match Win::new() {
+        Ok(w) => w,
+        Err(e) => {
+            eprintln!("Win oopsie: {e}");
+            return;
+        }
     };
-    log::debug!("Pixel Chunk Dims: ({}, {})", pixel_dims.width, pixel_dims.height);
+    // wins.push(win);
 
-    let mut win = Window::new(
-        "surmise", 
-        WINDOW_WIDTH, WINDOW_HEIGHT, 
-        WindowOptions { 
-            borderless: true, resize: true, transparency: true, ..WindowOptions::default()
-        },
-    ).unwrap(); 
+    // while wins.len() > 0 {
 
-    while win.is_open() && !win.is_key_down(Key::Escape) {
-        let (win_width, win_height) = win.get_size();
-        let window_dims: Rect = Rect {
-            width: win_width as u32, 
-            height: win_height as u32,
-        };
+    // }
 
-        if win.is_key_pressed(Key::Down, KeyRepeat::No) && pixel_dims.height > 1 {
-            pixel_dims.height = pixel_dims.height / 2;
-        } else if win.is_key_pressed(Key::Up, KeyRepeat::No) && pixel_dims.height < window_dims.height/2 {
-            pixel_dims.height = pixel_dims.height * 2; 
-        }
-        if win.is_key_pressed(Key::Right, KeyRepeat::No) && pixel_dims.width > 1 {
-            pixel_dims.width = pixel_dims.width / 2;
-        } else if win.is_key_pressed(Key::Left, KeyRepeat::No) && pixel_dims.width < window_dims.width/2 {
-            pixel_dims.width = pixel_dims.width * 2; 
-        }
-
+    loop {
         let new_camera_buffer = match camera.next() {
             Ok(b) => b,
             Err(e) => {
@@ -61,36 +40,9 @@ fn main() {
             }
         };
 
-        let source_dims: Rect = Rect {
-            width: new_camera_buffer.width(), 
-            height: new_camera_buffer.height(),
-        };
-        log::debug!("Og Image Dims: ({}, {})", source_dims.width, source_dims.height);
-
-        let (chunk_matrix, chunk_dims) = match calc_source_chunk_dims(
-            source_dims, window_dims as Rect, pixel_dims,
-        ) {
-            Ok(c) => c,
-            Err(e) => {
-                eprintln!("Chunking oopsie: {e}");
-                return;
-            }
-        };
-        log::debug!("Chunk Dims: ({}, {})", chunk_dims.width, chunk_dims.height);
-        log::debug!("Chunk Matrix: ({}, {})", chunk_matrix.width, chunk_matrix.height);
-
-        let downsampled = downsample(
-            new_camera_buffer, 
-            window_dims, pixel_dims,
-            chunk_matrix, chunk_dims, 
-            average::average,
-        );
-
-        let update_buffer = rbg_image_to_u32(&downsampled);
-        
-        win.update_with_buffer(
-            update_buffer.as_slice(), 
-            win_width, win_height,
-        ).unwrap();
+        let ok=  win.step(new_camera_buffer);
+        if !ok {
+            break;
+        }
     }
 }
--- src/window.rs
+++ src/window.rs
@@ -1,25 +1,124 @@
+use minifb::*;
+
+use anyhow::{Result};
+use image::{RgbImage};
+use crate::transform::{Rect, calc_source_chunk_dims, downsample, rbg_image_to_u32, average};
+
+
 pub struct Win {
-    win: Window
+    window: Window,
+    window_dims: Rect,
+    pixel_chunk: Rect,
 }
 
 impl Win {
-    pub fn new() -> Result<Window> {
+    pub fn new() -> Result<Win> {
         const WINDOW_WIDTH: usize = 960;
         const WINDOW_HEIGHT: usize = 540;
         log::debug!("Window Dims: ({}, {})", WINDOW_WIDTH, WINDOW_HEIGHT);
 
-        let mut pixel_dims: Rect = Rect {
+        let pixel_chunk: Rect = Rect {
             width: 32,
             height: 16,
         };
-        log::debug!("Pixel Chunk Dims: ({}, {})", pixel_dims.width, pixel_dims.height);
+        log::debug!("Pixel Chunk Dims: ({}, {})", pixel_chunk.width, pixel_chunk.height);
 
-        let mut win = Window::new(
+        let window = Window::new(
             "surmise", 
             WINDOW_WIDTH, WINDOW_HEIGHT, 
             WindowOptions { 
                 borderless: true, resize: true, transparency: true, ..WindowOptions::default()
             },
-        ).unwrap(); 
+        )?; 
+
+        Ok( Win {
+            window,
+            window_dims: Rect { width: WINDOW_WIDTH as u32, height: WINDOW_HEIGHT as u32 },
+            pixel_chunk
+        })
+    }
+
+    pub fn step(&mut self, raw_buf: RgbImage) -> bool {
+        if self.should_close() {
+            return false;
+        }
+
+        self.update_win_and_pix_size();
+
+        let raw_dims: Rect = Rect {
+            width: raw_buf.width(), 
+            height: raw_buf.height(),
+        };
+        log::debug!("Raw Image Dims: ({}, {})", raw_dims.width, raw_dims.height);
+
+        let (chunk_matrix, chunk_dims) = match calc_source_chunk_dims(
+            raw_dims, self.window_dims, self.pixel_chunk,
+        ) {
+            Ok(c) => c,
+            Err(e) => {
+                eprintln!("Chunking oopsie: {e}");
+                return true;
+            }
+        };
+        log::debug!("Chunk Dims: ({}, {})", chunk_dims.width, chunk_dims.height);
+        log::debug!("Chunk Matrix: ({}, {})", chunk_matrix.width, chunk_matrix.height);
+
+        let downsampled = downsample(
+            raw_buf, 
+            self.window_dims, self.pixel_chunk,
+            chunk_matrix, chunk_dims, 
+            average::average,
+        );
+
+        let update_buffer = rbg_image_to_u32(&downsampled);
+        
+        match self.window.update_with_buffer(
+            update_buffer.as_slice(), 
+            self.window_dims.width as usize, self.window_dims.height as usize,
+        ) {
+            Ok(u) => u,
+            Err(e) => {
+                eprintln!("Update oopsie: {e}");
+                return true;
+            } 
+        };
+
+        true
+    }
+
+    fn should_close(&mut self) -> bool {
+        if !self.window.is_open() || self.window.is_key_down(Key::Escape) {
+            return true;
+        }
+
+        false
+    }
+
+    fn update_win_and_pix_size(&mut self) {
+        let (win_width, win_height) = self.window.get_size();
+        self.window_dims = Rect {
+            width: win_width as u32, 
+            height: win_height as u32,
+        };
+
+        if self.window.is_key_pressed(Key::Down, KeyRepeat::No) && self.pixel_chunk.height > 1 {
+            self.pixel_chunk.height = self.pixel_chunk.height / 2;
+        } else if self.window.is_key_pressed(Key::Up, KeyRepeat::No) && self.pixel_chunk.height < self.window_dims.height / 2 {
+            self.pixel_chunk.height = self.pixel_chunk.height * 2; 
+        }
+
+        if self.window.is_key_pressed(Key::Right, KeyRepeat::No) && self.pixel_chunk.width > 1 {
+            self.pixel_chunk.width = self.pixel_chunk.width / 2;
+        } else if self.window.is_key_pressed(Key::Left, KeyRepeat::No) && self.pixel_chunk.width < self.window_dims.width / 2 {
+            self.pixel_chunk.width = self.pixel_chunk.width * 2; 
+        }
+
+        if self.window.is_key_pressed(Key::LeftBracket, KeyRepeat::No) && self.pixel_chunk.width > 1 && self.pixel_chunk.height > 1 {
+            self.pixel_chunk.width = self.pixel_chunk.width / 2;
+            self.pixel_chunk.height = self.pixel_chunk.height / 2;
+        } else if self.window.is_key_pressed(Key::RightBracket, KeyRepeat::No) && self.pixel_chunk.width < self.window_dims.width / 2 && self.pixel_chunk.height < self.window_dims.height / 2 {
+            self.pixel_chunk.width = self.pixel_chunk.width * 2; 
+            self.pixel_chunk.height = self.pixel_chunk.height * 2; 
+        }
     }
 }
