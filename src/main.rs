e877a2aebcb0dad3ea4deb0e5e468491b38067ed
ACK101101 <alexanderckumar@gmail.com>
2026-06-23 09:21:34 -0400

    first step at wins

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use anyhow::{Context, Result, bail};
+
 mod window;
 use window::*;
 
@@ -19,21 +21,15 @@ fn main() {
         }
     };
 
-    // let wins: Vec<Win> = Vec::new();
-    let mut win = match Win::new() {
+    let mut windows = match Windows::new() {
         Ok(w) => w,
         Err(e) => {
-            eprintln!("Win oopsie: {e}");
+            eprintln!("Windows oopsie: {e}");
             return;
         }
     };
-    // wins.push(win);
 
-    // while wins.len() > 0 {
-
-    // }
-
-    loop {
+    while !windows.wins.is_empty() {
         let new_camera_buffer = match camera.next() {
             Ok(b) => b,
             Err(e) => {
@@ -42,9 +38,43 @@ fn main() {
             }
         };
 
-        let ok = win.step(new_camera_buffer);
-        if !ok {
-            break;
+        let mut to_shutter_idxs: Vec<usize> = Vec::new();
+        for (idx, win) in windows.wins.iter_mut().enumerate() {
+            let ok = win.step(new_camera_buffer.clone());
+            if !ok {
+                to_shutter_idxs.push(idx);
+            }
         }
+
+        for idx in to_shutter_idxs.into_iter().rev() {
+            windows.shutter(idx).expect("shutter borked, idx wrong?");
+        }
+    }
+}
+
+struct Windows {
+    wins: Vec<Win>,
+}
+
+impl Windows {
+    fn new() -> Result<Windows> {
+        let win = Win::new().context("Win oopsie")?;
+        Ok(Windows { wins: vec![win] })
+    }
+
+    fn open(&mut self) -> Result<()> {
+        let win = Win::new().context("Win oopsie")?;
+        self.wins.push(win);
+
+        Ok(())
+    }
+
+    fn shutter(&mut self, idx: usize) -> Result<()> {
+        if idx >= self.wins.len() {
+            bail!("idx is out of bounds");
+        }
+
+        self.wins.remove(idx);
+        Ok(())
     }
 }
